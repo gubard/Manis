066f885cbc897fdfd11f0f2acce410b1c8b226d3
gubard <maksimovsergey08@gmail.com>
2026-01-07 12:29:40 +0200

    Update model builder and entity type to modify `Guid` and `DateOnly` comparers

--- Manis/CompiledModels/ManisDbContextModelBuilder.cs
+++ Manis/CompiledModels/ManisDbContextModelBuilder.cs
@@ -15,7 +15,7 @@ namespace Manis.CompiledModels
     public partial class ManisDbContextModel
     {
         private ManisDbContextModel()
-            : base(skipDetectChanges: false, modelId: new Guid("67b2318e-a6c4-4b4b-87af-fe9159d8c982"), entityTypeCount: 3)
+            : base(skipDetectChanges: false, modelId: new Guid("44aeff0e-f1b1-4a95-9424-080eba8b701c"), entityTypeCount: 3)
         {
         }
 
