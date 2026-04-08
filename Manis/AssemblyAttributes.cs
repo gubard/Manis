db338cbd1727b4ff2c2b4a55e3dbefe17af8fd77
gubard <maksimovsergey08@gmail.com>
2026-04-08 11:24:42 +0300

    Remove `AdoAuthenticationService` and related ADO assembly attributes.

--- Manis/AssemblyAttributes.cs
+++ Manis/AssemblyAttributes.cs
@@ -2,8 +2,6 @@
 using Nestor.Db.LiteDb.Models;
 using Nestor.Db.Models;
 
-[assembly: Ado(typeof(UserEntity), nameof(UserEntity.Id), false)]
-[assembly: AdoSourceEntity(typeof(UserEntity), nameof(UserEntity.Id))]
 [assembly: EditModel(typeof(UserEntity), nameof(UserEntity.Id))]
 [assembly: LiteDb(typeof(UserEntity), nameof(UserEntity.Id), false)]
 [assembly: LiteDbSourceEntity(typeof(UserEntity), nameof(UserEntity.Id))]
